6528c6c00c306ed09beb21dad6af4739a3afc4be
nareshmurugan <55019599+nareshmurugan@users.noreply.github.com>
2023-07-27 15:03:47 +0530

    Update milestone2_3.java

--- milestone2_3.java
+++ milestone2_3.java
@@ -14,13 +14,13 @@ public class milestone2_3 {
 
     public static int repeated(int[]input1,int input2) {
         int res=0;
-        for (int i=0;i<input1.length;i++)if(isprime(input1[i]))res+=i;
+        for (int i=0;i<input1.length;i++)if(!isprime(input1[i]))res+=i;
         return res;
     }
 
     public static boolean isprime(int a) {
         if(a<2)return false;
-        for(int i=2;i<=a/2;i++)if(a%i==0)return false;
+        for(int i=2;i<a;i++)if(a%i==0)return false;
         return true;
     }
 }
